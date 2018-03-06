6c1a265b7703c7532b1143cd7ee9bb5f091d62c7
unknown <cyril.caron95@gmail.com>
2018-03-06 17:51:16 +0100

    Update with promises && Stop using local array

--- IPFS_VersionControl.js
+++ IPFS_VersionControl.js
@@ -3,72 +3,90 @@
 
 
 const readline = require('readline');
+const { exec } = require('child_process');
+var PouchDB = require('pouchdb');
+
+var db = new PouchDB('programs');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const { exec } = require('child_process');
-var PouchDB = require('pouchdb');
-var db = new PouchDB('programs');
 
-var programs = [];
-
-function fileAlreadyExists(file_name, programs) {//returns -1 if the file doesn't exists, the index of the file if it exists
-    var index = 0;
-    for (var index = 0; index < programs.length; index++) {
-        if (programs[index].name == file_name)
-            return index;
-    }
-
-    return -1;
+function fileAlreadyExistsInDatabase(file_name) { //Try to retrieve the document in the db to see if the file exists.
+    return new Promise((resolve, reject) => {
+        db.get(file_name)
+            .then((doc) => {
+                console.log("The following program already exists :\n\n" + JSON.stringify(doc));
+                return resolve(0);
+            })
+            .catch((err) => {
+                console.log("The program doesn't exist yet !\n\n");
+                return resolve(-1);
+            })
+    })
 }
+
 function showPrograms() {
-  db.allDocs({include_docs: true, descending: true}, function(err, doc) {
-  //  redrawTodosUI(doc.rows);
-      console.log(JSON.stringify(doc.rows, null, 4));
-  });
+    return new Promise((resolve, reject) => {
+        db.allDocs({ include_docs: true, descending: true })
+            .then((doc) => {
+                console.log(JSON.stringify(doc.rows, null, 4));
+                return resolve()
+            })
+    })
 }
+
 
 var recursiveAsyncReadLine = function () {
 
-    rl.question("What do you want to do : \n 1-Add new file to IPFS \n 2-Look at the IPFS files\n", (answer) => {
+    rl.question("\n\n What do you want to do : \n\n 1-Add new file to IPFS \n 2-Look at the IPFS files\n\n", (answer) => {
 
         switch (answer.trim()) {
             case "1":
-                var program = { "name": "", "versions": [] };
+                var program = { "_id": "", "versions": [] };
                 var version = { "number": "", "hash": "" };
-                rl.question("Enter the file path you want to add to IPFS :\n", (path) => {
+                rl.question("Enter the file path you want to add to IPFS :\n\n", (path) => {
                     var command = "ipfs add " + path.trim();
-
+        
                     exec(command, (err, stdout, stderr) => {
-                        if (err) {
-                            console.error(err);
-                            return;
-                        }
-                        console.log(stdout);
-                        var arrayOfSubstrings = stdout.split(" ");
-                        var file_name = arrayOfSubstrings[2].replace("\n", "");
-                        var file_hash = arrayOfSubstrings[1];
-                        var exists = fileAlreadyExists(file_name, programs);
-                        rl.question("Enter the file version :\n", (file_version) => {
-                            version.number = file_version;
-                            version.hash = file_hash;
-                            if (exists != -1) {//if the file already exists
-                                programs[exists].versions.push(version);
+                        if (!err) {
 
-                            }
-                            else {//if the files doesn't exists
-                                program.name = file_name;
-                                program.versions.push(version);
-                                programs.push(program);
-                            }
-                            db.post(program, function callback(err, result) {
-                            if (!err) {
-                              console.log('Successfully posted the program !');
-                            }
-                          });
-                setTimeout(recursiveAsyncReadLine,1000);//calls back the function after a second to post the program to the database first
-                        });
+                            console.log(stdout);
+                            var arrayOfSubstrings = stdout.split(" ");
+                            var file_name = arrayOfSubstrings[2].replace("\n", "");
+                            var file_hash = arrayOfSubstrings[1];
+                            fileAlreadyExistsInDatabase(file_name)
+                                .then((exists) => {
+                                    
+                                    rl.question("Enter the file version :\n\n", (file_version) => {
+                                        version.number = file_version;
+                                        version.hash = file_hash;
+                                        if (exists != -1) {//if the file already exists
+                                            db.get(file_name)
+                                                .then((prog) => {
+                                                    prog.versions.push(version)
+                                                    return db.put(prog)
+                                                })
+                                                .then(console.log(`Succesfully add the version ${version.number} of the program ${file_name} ! `))
+                                                .catch((err) => console.log(err))
+                                        }
+                                        else {//if the files doesn't exists
+                                            program._id = file_name;
+                                            program.versions.push(version);
+                                            db.put(program)
+                                                .then(console.log(`Succefully added the program ${file_name} !`))
+                                                .catch((err) => console.log(err))
+                                        }
+                                        recursiveAsyncReadLine()//calls back the function after a second to post the program to the database first
+                                    })
+    
+                                });
+                        }
+                        else{
+                            console.error(err);
+                        }
+                     
                     });
 
                 });
@@ -77,14 +95,14 @@ var recursiveAsyncReadLine = function () {
 
 
             case "2":
-                   console.log("local programs :\n"+JSON.stringify(programs, null, 4));
-                console.log("pouchdb :");
-                showPrograms();
-                setTimeout(recursiveAsyncReadLine,1000);//calls back the function after a second to display the list of programs first
+                console.log("Your PouchDB Programs :\n\n");
+                showPrograms()
+                    .then(recursiveAsyncReadLine);
+
                 break;
 
             default:
-                console.log("wrong input");
+                console.log("Wrong input\n\n");
                 recursiveAsyncReadLine();
                 break;
         }
